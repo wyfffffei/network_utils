1cf97c4787099361ab703accc53184345619741a
wyfffffei <h3wyf@126.com>
2023-01-19 15:45:53 +0800

    translate data in file

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 
 网络工具包。
 
-
-
-## conf_parser.py
-
-解析配置文件到 `dict` 或者 `json` 格式。
-
 依赖（可选）：
 
 ```bash
 pip install tqdm
 ```
+
+
+
+## conf_parser.py
+
+解析配置文件到 `dict` 或者 `json` 格式。
 
 - 飞塔防火墙
 
@@ -29,5 +29,32 @@ if __name__ == "__main__":
 
     from tools import dict2json
     dict2json(p, out_path)
+```
+
+
+
+## translator.py
+
+翻译 `api` 调用器。
+
+- 百度 API
+
+```python
+# 主函数部分
+if __name__ == "__main__":
+    # 从文件读取 appid 和 secretKey
+    with open("doc/bt_app_sec.txt", "r", encoding="utf-8") as f:
+        appid, secretKey = f.readlines()[0].strip().split(",")
+    
+    bt = Baidu_Translator(appid, secretKey)
+    
+    # 翻译单词
+    res = bt.translate("banana")
+    print(res)
+
+    # 翻译文件（逐行翻译，逐行输出）
+    res = bt.translate_file("doc/words.txt", is_save=True)
+    with open("doc/output.txt", "w", encoding="utf-8") as f:
+        f.writelines([w + "\n" for w in res])
 ```
 
--- translator.py
+++ translator.py
@@ -4,6 +4,8 @@ import urllib
 import random
 import json
 import time
+import os
+from tqdm import tqdm
 
 
 class Baidu_Translator:
@@ -16,7 +18,6 @@ class Baidu_Translator:
         self.salt = random.randint(32768, 65536)
 
     def translate(self, q, fromLang="auto", toLang="zh") -> dict:
-        time.sleep(1)
         sign = self.appid + q + str(self.salt) + self.secretKey
         sign = hashlib.md5(sign.encode()).hexdigest()
         url = self.url + '?appid=' + self.appid + '&q=' + urllib.parse.quote(q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(
@@ -38,6 +39,28 @@ class Baidu_Translator:
             if httpClient:
                 httpClient.close()
 
+    def translate_file(self, path, fromLang="auto", toLang="zh", is_save=False) -> list:
+        if not os.path.exists(path):
+            print("File path is invalid.")
+            return None
+    
+        with open(path, 'r', encoding="utf-8") as f:
+            lines = [line.strip() for line in f.readlines()]
+            words = set(lines)
+        
+        brain = {}
+        for word in tqdm(words):
+            time.sleep(1)
+            ans = self.translate(word, fromLang=fromLang, toLang=toLang)
+            brain[word] = ans["trans_result"][0]["dst"]
+        
+        if is_save:
+            # save the dict translation with json
+            from tools import dict2json
+            dict2json(brain, "doc/translation_{}.json".format(time.strftime("%Y%m%d%H%M%S", time.localtime())))
+        
+        return [brain[line] for line in lines]
+
 
 if __name__ == "__main__":
     # read the key
@@ -45,5 +68,12 @@ if __name__ == "__main__":
         appid, secretKey = f.readlines()[0].strip().split(",")
     
     bt = Baidu_Translator(appid, secretKey)
-    res = bt.translate("banana")
-    print(res)
+    
+    # 翻译单词
+    # res = bt.translate("banana")
+    # print(res)
+
+    # 翻译文件（逐行翻译，逐行输出）
+    res = bt.translate_file("doc/words.txt", is_save=True)
+    with open("doc/output.txt", "w", encoding="utf-8") as f:
+        f.writelines([w + "\n" for w in res])

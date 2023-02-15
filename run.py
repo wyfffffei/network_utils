b58da8c33d00a46a6d32039967ba17f63f096b3a
wyfffffei <h3wyf@126.com>
2023-02-16 01:53:42 +0800

    improve parameter input

--- run.py
+++ run.py
@@ -14,19 +14,18 @@ if __name__ == "__main__":
         description=
         """
         cheatsheet:
-        python run.py -p englist_words.txt -t path_of_key.txt  (translator)
+        python run.py -p english_words.txt -t path_of_key.txt  (translator)
         python run.py -p cfgbackup_ft_fw01.conf -d ft  (config parser)
-        python run.py -p command_lines.txt -c -na root -ip 192.168.56.102  (command sender to remote)
+        python run.py -p command_lines.txt -i 192.168.56.102 -u root (command sender)
         """,
         formatter_class=RawFormatter
         )
-    parser.add_argument("-p", "--path", type=str, required=True, help="englist words / config / command path (required)")
+    parser.add_argument("-p", "--path", type=str, required=True, help="english words / config / command path (required)")
     parser.add_argument("-t", "--translate", type=str, help="secretkey file path(content: 'appid,secretkey'), translate file line-by-line")
     parser.add_argument("-d", "--device", type=str, choices=["ft", "cp"], help="network device: ft or cp")
-    parser.add_argument("-c", "--connection", action="store_true", help="send commands to the remote")
-    parser.add_argument("-na", "--username", type=str, help="remote username")
-    parser.add_argument("-ip", "--ipaddress", type=str, help="remote ip address")
-    parser.add_argument("-pt", "--port", type=int, default=22, help="remote port")
+    parser.add_argument("-i", "--ipaddress", type=str, help="remote ip address")
+    parser.add_argument("-u", "--username", type=str, help="remote username")
+    parser.add_argument("--port", type=int, default=22, help="remote port")
     parser.add_argument("--password", type=str, help="remote password (unrecommanded)")
     parser.add_argument("--en-password", type=str, help="remote enable password (unrecommanded)")
     arg = parser.parse_args()
@@ -38,7 +37,7 @@ if __name__ == "__main__":
                 appid, secretKey = f.readlines()[0].strip().split(",")
         except Exception as e:
             print(e)
-            print("secretkey file path is invalid")
+            print("secretkey file is invalid")
             sys.exit(-1)
         bt = Baidu_Translator(appid, secretKey)
         res = bt.translate_file(arg.path, is_save=True)
@@ -53,9 +52,9 @@ if __name__ == "__main__":
             dict2json(CheckPoint(arg.path).parse_configuration(), output_path)
         print("save -> {}_parsed.json".format(arg.path))
 
-    elif arg.connection:
-        if not arg.ipaddress or not arg.username:
-            print("Username or password not found.")
+    elif arg.ipaddress:
+        if not arg.username:
+            print("Username not found.")
             sys.exit(-1)
         
         from network_utils.connection import Connection
@@ -73,8 +72,9 @@ if __name__ == "__main__":
             "Enter expert password:": en_password,
             "Are you sure you want to continue?(Y/N)[N]": "y"
         }
-        print("Start receiving message:")
+        print("Start receiving:")
         print(30 * '-')
         for stdout in client.send_command_file(arg.path, confirm_flag=confirm_flag):
             print(stdout, end="")
         client.close()
+

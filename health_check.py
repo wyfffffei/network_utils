1836c5d1f294231b8b10bb17acee34cbd045499f
wyfffffei <h3wyf@126.com>
2024-02-01 16:46:57 +0800

    dns检测 & 域名请求返回码检测

--- health_check.py
+++ health_check.py
@@ -2,15 +2,17 @@
 
 # ref: https://github.com/rthalley/dnspython/blob/master/examples/ddr.py
 
+import sys
+import argparse
+
 import requests
 import dns.resolver
-import pandas as pd
-from tqdm import tqdm
 
 
-def get_dns_records(domain):
+def get_dns_records(domain, dns_server=["223.5.5.5", "114.114.114.114"]):
     res = dns.resolver.Resolver(configure=False)
-    res.nameservers = ["114.114.114.114", "8.8.8.8"]
+    res.nameservers = dns_server
+    print("DNS Server: {}".format(str(dns_server)))
     res.try_ddr()
     ANSWER = {"A": [], "CNAME": []}
 
@@ -27,7 +29,7 @@ def get_dns_records(domain):
     return ANSWER
 
 
-def check_website_status(protocal, domain, timeout=5):
+def check_website_status(domain, protocal="https", timeout=5):
     try:
         # 发送HTTP HEAD请求来检查网站状态
         response = requests.head(protocal + "://" + domain, timeout=timeout)
@@ -36,6 +38,39 @@ def check_website_status(protocal, domain, timeout=5):
         return e
 
 
-if __name__ == "__main__":
+def check_bandwidth(domain):
     pass
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="CS: py health_check.py -d baidu.com google.com -r -s")
+    parser.add_argument('-d', '--domains', required=True, type=str, nargs='+', help="")
+    parser.add_argument('-f', '--file', action="store_true", help="Domains parameter includes path of domain file.")
+    parser.add_argument('-r', '--resolve', action="store_true", help="Dns resolution.")
+    parser.add_argument('-s', '--web_status', action="store_true", help="Website status code detection.")
+    arg = parser.parse_args()
+
+    if not arg.resolve and not arg.web_status:
+        print("Invalid parameter given.")
+        sys.exit(-1)
+    
+    DOMAINS = arg.domains
+    if arg.file:
+        try:
+            with open(arg.file, 'r') as f:
+                DOMAINS = [line.strip().strip("\'\"") for line in f.readlines()]
+        except Exception:
+            print("Invalid file path given.")
+            sys.exit(-1)
+
+    for domain in DOMAINS:
+        print(domain)
+        if arg.resolve:
+            ret = get_dns_records(domain)
+            print(ret)
+        if arg.web_status:
+            ret = check_website_status(domain)
+            print(ret)
+        print()
 
